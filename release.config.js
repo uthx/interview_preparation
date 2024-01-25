9094dc96ee39a27742d17239af6e3a6c61264827
uthx <rdrahuldhiman.19@gmail.com>
2024-01-25 23:28:30 +0530

    feat: Introduce semantic release

---
+++ release.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["package.json", "CHANGELOG.md"],
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
+  ]
+} 
